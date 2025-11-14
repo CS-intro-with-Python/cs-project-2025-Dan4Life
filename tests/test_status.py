701d546c750f6fbe8112222199492572d5012a9d
Dan4Life <danielilozor2006@gmail.com>
2025-11-14 01:03:57 +0100

    Fix port handling 2

--- tests/test_status.py
+++ tests/test_status.py
@@ -1,5 +1,7 @@
 import requests
 
 def test_status():
-    r = requests.get("http://localhost:5000/api/status")
+    r = requests.get("http://localhost:8080/api/status") 
+    
+    # Assert that the status code is 200 (OK)
     assert r.status_code == 200
