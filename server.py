cfba652d932ed51351f9f8f5d69a9d12565fd02a
Dan4Life <danielilozor2006@gmail.com>
2026-01-14 20:16:33 +0200

    Finishing Touches

--- server.py
+++ server.py
@@ -229,7 +229,21 @@ def create_app():
         },
         "basePath": "/",
     }
-    Swagger(app, template=swagger_template)
+    swagger_config = {
+        "headers": [],
+        "specs": [
+            {
+                "endpoint": "apispec_1",
+                "route": "/docs/apispec_1.json",
+                "rule_filter": lambda rule: True,
+                "model_filter": lambda tag: True,
+            }
+        ],
+        "static_url_path": "/flasgger_static",
+        "swagger_ui": True,
+        "specs_route": "/docs/",
+    }
+    Swagger(app, template=swagger_template, config=swagger_config)
 
     @app.route("/")
     def index():
